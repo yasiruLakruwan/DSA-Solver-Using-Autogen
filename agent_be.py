94c87664ef0def16e9f33cefc2e3fdffbb4f1cf1
yasiruLakruwan <yapa1459@gmail.com>
2025-08-14 16:54:30 +0530

    commit

---
+++ agent_be.py
@@ -0,0 +1,9 @@
+from autogen_agentchat.agents import CodeExecutorAgent
+import asyncio
+from autogen_ext.code_executors.docker import DockerCommandLineCodeExecutor
+from autogen_agentchat.messages import TextMessage 
+
+
+
+
+
